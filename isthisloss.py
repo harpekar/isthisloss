6b6e17bbfbf364c824a3fe243dddf1af11cf45bb
Karen <karenl.harper88@gmail.com>
2018-08-20 19:11:14 -0700

    Using watershed algorithm for edge finding

--- isthisloss.py
+++ isthisloss.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+
 import cv2
 from PIL import Image
 import sys
@@ -10,25 +11,40 @@ outline = cv2.imread('loss-outline.jpg')
 #outline = cv2.cvtColor(outline, cv2.COLOR_BGR2GRAY) #grayscale outline image--make only one channel
 #outline = cv2.Canny(outline, 100, 200)
 
-img = cv2.imread(sys.argv[1]) #Read in user-provided image
+img = cv2.imread(sys.argv[1]) #Read in user-provided image as a numpy array
+
+mask = np.zeros(img.shape[:2],np.uint8)
 
 if img.any() == None:
     raise Exception("Could not load image")
 
 else:
-    graysc = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #grayscale image
-    #edges = cv2.Canny(graysc,100,200) #Finds edges in given image
-    ret, thresh = cv2.threshold(graysc, 0,100,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    height, width = img.shape[:2] #Get image dimensions
+    firstPanel = img[0:height/2, 0:width/2]
 
-    print (outline.shape)
-    print (outline.size)
-    print (outline.dtype)
+    first_img = Image.fromarray(firstPanel, "RGB")
+    
+#plt.imshow(first_img)
+#plt.show()
+
+    graysc = cv2.cvtColor(firstPanel, cv2.COLOR_BGR2GRAY) #grayscale image
+    #edges = cv2.Canny(graysc,100,200) #Finds edges in given image
+    ret, thresh = cv2.threshold(graysc, 0,150,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+    print (firstPanel.shape)
+    print (firstPanel.size)
+    print (firstPanel.dtype)
 
 #    print (edges.shape)
 #    print (edges.size)
 #    print (edges.dtype)
 
 #    output = cv2.addWeighted(edges, 1, outline, 1, 0) #Overlays template from original image onto picture
+
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+     
+#    plt.imshow(img),plt.colorbar(),plt.show()
 
     plt.imshow(thresh, cmap = 'gray')
     plt.show()
