96c6c3653e4aeff733fbcf61183421de439e130a
Karen <karenl.harper88@gmail.com>
2020-03-19 16:57:56 -0700

    Changed threshold for better object detection

--- isthisloss.py
+++ isthisloss.py
@@ -7,44 +7,54 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 #Prepare outline image
-outline = cv2.imread('loss-outline.jpg')
+outline = cv2.imread('loss.jpg', cv2.IMREAD_GRAYSCALE)
+
+
 #outline = cv2.cvtColor(outline, cv2.COLOR_BGR2GRAY) #grayscale outline image--make only one channel
 #outline = cv2.Canny(outline, 100, 200)
 
-img = cv2.imread(sys.argv[1]) #Read in user-provided image as a numpy array
-
-mask = np.zeros(img.shape[:2],np.uint8)
+img = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE) #Read in user-provided image as a numpy array
 
 if img.any() == None:
     raise Exception("Could not load image")
 
 else:
     height, width = img.shape[:2] #Get image dimensions
-    firstPanel = img[0:height/2, 0:width/2]
 
-    first_img = Image.fromarray(firstPanel, "RGB")
+    half_height = height/2
+    half_width = width/2
+
+    #to get more exact foreground/background detection, analyze each panel individually
+    first_panel = img[0:half_height, 0:half_width]
+    second_panel = img[0:half_height, half_width:width]
+    third_panel = img[half_height:height, 0:half_width]
+    fourth_panel = img[half_height:height, half_width:width] 
+
+    cv2.imshow("Outline", first_panel)
+
+    th, im_th = cv2.threshold(outline, 125, 255,cv2.THRESH_BINARY_INV)
+
+    im_floodfill = im_th.copy()
+
+    h, w = im_th.shape[:2]
+
+    mask = np.zeros((h+2, w+2), np.uint8)
+
+    # Floodfill from point (0, 0)
+
+    cv2.floodFill(im_floodfill, mask, (0,0), 255);
+
+    # Invert floodfilled image
+
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+
+    # Combine the two images to get the foreground.
+
+    im_out = im_th | im_floodfill_inv
+
+    # Display images.
+
     
-#plt.imshow(first_img)
-#plt.show()
+    cv2.imshow("Foreground", im_out)
 
-    graysc = cv2.cvtColor(firstPanel, cv2.COLOR_BGR2GRAY) #grayscale image
-    #edges = cv2.Canny(graysc,100,200) #Finds edges in given image
-    ret, thresh = cv2.threshold(graysc, 0,150,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-    print (firstPanel.shape)
-    print (firstPanel.size)
-    print (firstPanel.dtype)
-
-#    print (edges.shape)
-#    print (edges.size)
-#    print (edges.dtype)
-
-#    output = cv2.addWeighted(edges, 1, outline, 1, 0) #Overlays template from original image onto picture
-
-    bgdModel = np.zeros((1,65),np.float64)
-    fgdModel = np.zeros((1,65),np.float64)
-     
-#    plt.imshow(img),plt.colorbar(),plt.show()
-
-    plt.imshow(thresh, cmap = 'gray')
-    plt.show()
+    cv2.waitKey(0)
