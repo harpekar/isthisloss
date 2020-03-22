a619e1271d8138ac375286030f2c7bf1e634bf94
Karen <karenl.harper88@gmail.com>
2020-03-21 23:35:26 -0700

    Moved masking to separate function

--- isthisloss.py
+++ isthisloss.py
@@ -6,43 +6,43 @@ import sys
 import numpy as np
 from matplotlib import pyplot as plt
 
+def prepare_mask(image): 
+    
+    height, width = img.shape[:2] #Get image dimensions
+
+    th, im_th = cv2.threshold(outline, 125, 255,cv2.THRESH_BINARY_INV)
+
+    im_floodfill = im_th.copy()
+    
+    h, w = im_th.shape[:2]
+    
+    mask = np.zeros((h+2, w+2), np.uint8)
+    
+    # Floodfill from point (0, 0)
+    
+    cv2.floodFill(im_floodfill, mask, (0,0), 255);
+    
+    # Invert floodfilled image
+    
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+    
+    # Combine the two images to get the foreground.
+    
+    mask = im_th | im_floodfill_inv
+    
+    return mask    
+
+
 #Prepare outline image
 outline = cv2.imread('loss.jpg', cv2.IMREAD_GRAYSCALE)
 
-
-#outline = cv2.cvtColor(outline, cv2.COLOR_BGR2GRAY) #grayscale outline image--make only one channel
-#outline = cv2.Canny(outline, 100, 200)
 
 img = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE) #Read in user-provided image as a numpy array
 
 if img.any() == None:
     raise Exception("Could not load image")
 
-height, width = img.shape[:2] #Get image dimensions
-
-th, im_th = cv2.threshold(outline, 125, 255,cv2.THRESH_BINARY_INV)
-
-im_floodfill = im_th.copy()
-
-h, w = im_th.shape[:2]
-
-mask = np.zeros((h+2, w+2), np.uint8)
-
-# Floodfill from point (0, 0)
-
-cv2.floodFill(im_floodfill, mask, (0,0), 255);
-
-# Invert floodfilled image
-
-im_floodfill_inv = cv2.bitwise_not(im_floodfill)
-
-# Combine the two images to get the foreground.
-
-mask = im_th | im_floodfill_inv
-
-# Display images.
-
-
+mask = prepare_mask(img)
     
 cv2.imshow("Foreground", mask)
 
